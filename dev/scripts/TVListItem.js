46c9182cb7d34436e5ab60c79db50514e7c76e77
Nishi Rathod <nishi85@outlook.com>
2018-05-06 23:58:19 -0400

    minor stylings

--- dev/scripts/TVListItem.js
+++ dev/scripts/TVListItem.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Link } from "react-router-dom";
 
+
 const TVListItem = (show) => {
   const imgUrl = `https://image.tmdb.org/t/p/w342/${show.img}`;
 
   return (
     <li className="TVitem">
-         <Link to={`/shows/${show.id}`} className="thumbnail">
+        <Link to={`/shows/${show.id}`} className="thumbnail">
         <img src={imgUrl} alt="" />
         <div className="show-description">
           <h3>{show.name}</h3>
